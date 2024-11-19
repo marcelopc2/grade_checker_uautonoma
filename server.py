2919a353a3606fe3455e35e02883dc402e557deb
marcelopc2 <marcelopc2@gmail.com>
2024-11-19 16:53:33 -0300

    actualizado el requirements.txt

--- server.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-
-if __name__ == "__main__":
-    # Cambia "app.py" por el nombre de tu archivo principal de Streamlit
-    subprocess.run(["streamlit", "run", "main.py", "--server.port", os.environ.get("PORT", "8501"), "--server.headless", "true"])
